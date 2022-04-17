0425ec50059e96427f02be3e4468c04cb90d747d
Shubham Suresh Borade <spsb36@gmail.com>
2022-04-17 19:50:47 +0530

    final

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,16 @@
 
 import { Button } from '@mui/material'
 import React from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
 
     const navigate = useNavigate();
 
-
     return (
         <>
             <h3 style={{ fontFamily: "Serif", color: "#8F7FD2" }}>Welcome</h3>
-            <Button variant='contained' onClick={()=>navigate("/admin")}>Admin log in</Button>
+            <Button variant='contained' onClick={() => navigate("/admin")}>Admin log in</Button>
 
         </>
     )
